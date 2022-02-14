5399a16d93fb1a12469d4d56191847b854f04abc
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-14 16:08:38 +0000

    Added /lib and /views

--- rps.rb
+++ rps.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require './lib/game'
 
 get '/' do
-  'Rock, paper, sciss!'
+  erb(:index)
+end
+
+post '/names' do
+  @game = Game.create(params[:player_1_name])
+  redirect '/play'
+end
+
+get '/play' do
+  @game = Game.instance
+  erb(:play)
+end
+
+post '/play' do
+  @game = Game.instance
+  @game.player_turn(params[:choice])
+  @game.computer_turn
+  redirect '/play'
 end
